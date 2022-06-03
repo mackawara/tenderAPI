d13fdae115c289e218e0ad3a735110fbdc0cc8bd
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-02 21:41:56 -0400

    changed server side stufff cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const app= express()
 const port= 2000|| process.env.PORT
+const cors=require("cors")
+app.use(cors)
 
 app.listen(port,()=>console.log("server listening on port"+ port))
 
