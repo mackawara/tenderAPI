45b6dd2bde006ee8fb5d7c97eaf71e87a231dd61
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-04 20:58:25 +0200

    added express router dependencies

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-const express=require("express")
-const app= express()
-const port= 2000|| process.env.PORT
-const cors=require("cors")
-app.use(cors)
+const express = require("express");
+const app = express();
+const PORT = 2000 || process.env.PORT;
+const cors = require("cors");
+const router=require("express-router)
+app.use(cors);
 
-app.listen(port,()=>console.log("server listening on port"+ port))
-
-app.get("/", (req,res)=>{
-    res.send("Hello World")
-})
+app.listen(PORT, () => console.log("server listening on port" + PORT));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
